f8bd590c32fb05acf69334acc80bbb2d82575df7
chivicoko <victor.c.okoye@gmail.com>
2025-01-05 15:59:39 +0100

    update projects

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,17 +1,17 @@
 
-export const leads = [
-    { id: 1, name: "Winfor Asher", topic: 'Cafe A100 for commercial use', statusReason: 'New', createdOn: '2/01/2024 1:00 PM' },
-    { id: 2, name: "Josia Love", topic: 'Upgrading service plan', statusReason: 'New', createdOn: '4/02/2024 12:00 PM' },
-    { id: 3, name: "Harrison Curtis", topic: 'Issue with throughput on EspressoMaster', statusReason: 'New', createdOn: '22/02/2024 11:00 PM' },
-    { id: 4, name: "Jermaine Berrett", topic: 'New roaster in distribution facility', statusReason: 'New', createdOn: '4/03/2024 12:00 PM' },
-    { id: 5, name: "Gerald Stephens", topic: 'Concerns on current machines', statusReason: 'New', createdOn: '4/04/2024 12:40 PM' },
-    { id: 6, name: "Halle Griffiths", topic: 'Expanding business', statusReason: 'New', createdOn: '14/05/2024 10:25 PM' },
-    { id: 7, name: "Rachel Michael", topic: 'Addressing service concerns', statusReason: 'New', createdOn: '21/03/2024 12:00 AM' },
-    { id: 8, name: "Alex Baker", topic: 'Premium coffee beans', statusReason: 'New', createdOn: '24/01/2024 2:15 AM' },
-    { id: 9, name: "Lily Pyles", topic: 'Cafe A100 bulk rate', statusReason: 'New', createdOn: '6/06/2024 9:00 AM' },
-    { id: 10, name: "Jane Reyes", topic: 'Improving cost per cup', statusReason: 'New', createdOn: '4/07/2024 12:00 PM' },
-    { id: 11, name: "Mike Pears", topic: 'Improving cost per spoon', statusReason: 'New', createdOn: '5/07/2024 1:00 PM' },
-]
+// export const leads = [
+//     { id: 1, name: "Winfor Asher", topic: 'Cafe A100 for commercial use', statusReason: 'New', createdOn: '2/01/2024 1:00 PM' },
+//     { id: 2, name: "Josia Love", topic: 'Upgrading service plan', statusReason: 'New', createdOn: '4/02/2024 12:00 PM' },
+//     { id: 3, name: "Harrison Curtis", topic: 'Issue with throughput on EspressoMaster', statusReason: 'New', createdOn: '22/02/2024 11:00 PM' },
+//     { id: 4, name: "Jermaine Berrett", topic: 'New roaster in distribution facility', statusReason: 'New', createdOn: '4/03/2024 12:00 PM' },
+//     { id: 5, name: "Gerald Stephens", topic: 'Concerns on current machines', statusReason: 'New', createdOn: '4/04/2024 12:40 PM' },
+//     { id: 6, name: "Halle Griffiths", topic: 'Expanding business', statusReason: 'New', createdOn: '14/05/2024 10:25 PM' },
+//     { id: 7, name: "Rachel Michael", topic: 'Addressing service concerns', statusReason: 'New', createdOn: '21/03/2024 12:00 AM' },
+//     { id: 8, name: "Alex Baker", topic: 'Premium coffee beans', statusReason: 'New', createdOn: '24/01/2024 2:15 AM' },
+//     { id: 9, name: "Lily Pyles", topic: 'Cafe A100 bulk rate', statusReason: 'New', createdOn: '6/06/2024 9:00 AM' },
+//     { id: 10, name: "Jane Reyes", topic: 'Improving cost per cup', statusReason: 'New', createdOn: '4/07/2024 12:00 PM' },
+//     { id: 11, name: "Mike Pears", topic: 'Improving cost per spoon', statusReason: 'New', createdOn: '5/07/2024 1:00 PM' },
+// ]
 
 export const skills = [
     {
@@ -156,6 +156,28 @@ export const projects = [
             { id: 1, img: "/images/tsup-chat/tSup-chat-1.png" },
             { id: 2, img: "/images/tsup-chat/tSup-chat-2.png" },
             { id: 3, img: "/images/tsup-chat/tSup-chat-3.png" },
+        ]
+    },
+    { 
+        id: 7,
+        name: 'Sweettooth Mini Store',
+        description: 'This is a chat application.',
+        img: "/images/sweettooth/sweettooth-1.png",
+        githubLink: 'https://github.com/chivicoko/sweettooth',
+        liveDemoLink: 'https://sweettooth-steel.vercel.app/',
+        techStack: 'ReactJS, Typescript, Vite, SASS, MUI, Timbu Cloud API',
+        otherImages: [
+            { id: 1, img: "/images/sweettooth/sweettooth-1.png" },
+            { id: 2, img: "/images/sweettooth/sweettooth-2.png" },
+            { id: 3, img: "/images/sweettooth/sweettooth-3.png" },
+            { id: 4, img: "/images/sweettooth/sweettooth-4.png" },
+            { id: 5, img: "/images/sweettooth/sweettooth-5.png" },
+            { id: 6, img: "/images/sweettooth/sweettooth-6.png" },
+            { id: 7, img: "/images/sweettooth/sweettooth-7.png" },
+            { id: 8, img: "/images/sweettooth/sweettooth-8.png" },
+            { id: 9, img: "/images/sweettooth/sweettooth-9.png" },
+            { id: 10, img: "/images/sweettooth/sweettooth-10.png" },
+            { id: 11, img: "/images/sweettooth/sweettooth-11.png" },
         ]
     },
 ]
