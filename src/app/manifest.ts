ac36ee112fcc35b6f52ae7aa87c000e3873d39a0
chivicoko <victor.c.okoye@gmail.com>
2025-02-09 03:22:00 +0100

    update manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -2,9 +2,9 @@ import type { MetadataRoute } from 'next'
  
 export default function manifest(): MetadataRoute.Manifest {
   return {
-        "name": "VictorOkoye.com",
-        "short_name": "VictorOkoye.com",
-        "description": "VictorOkoye.com is the personal website/portfolio of Victor Okoye, an expert Full-stack Web Developer.",
+        "name": "VictorOkoye",
+        "short_name": "VC",
+        "description": "VictorOkoye (VC) is the personal website/portfolio of Victor Okoye, an expert Full-stack Web Developer.",
         "icons":[
             {"src":"/icon-16x16.png","sizes":"16x16","type":"image/png"},
             {"src":"/icon-32x32.png","sizes":"32x32","type":"image/png"},
