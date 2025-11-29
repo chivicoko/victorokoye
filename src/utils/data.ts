59af4f171270e8f95e30a64479b6e5268f2551ff
chivicoko <victor.c.okoye@gmail.com>
2025-11-29 16:18:54 +0100

    update data

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -201,7 +201,6 @@ export const experiences = [
         company: 'Optisource Technology Solutions',
         position: 'Front-end Engineer',
         duration: 'April, 2025 — Present',
-        // duration: 'April, 2023 — June, 2025',
         points: [
             "Work as a Front-end Engineer specializing in React.js as the primary technology stack.",
             "Contribute to the development of scalable B2B technology solutions that help businesses streamline their operations.",
@@ -211,9 +210,20 @@ export const experiences = [
     },
     {
         id: 2,
+        company: 'DEV47 Agency',
+        position: 'Front-end Engineer',
+        duration: 'November, 2024 - February, 2025',
+        points: [
+            "Solo Front-end Engineer - contribute in developing scalable technology solutions for organizations.",
+            "Websites such as https://wearsbylegend.com/ - an eCommerce website for body wears.",
+        ],
+        verifyLink: "https://wearsbylegend.com/"
+    },
+    {
+        id: 3,
         company: 'Stealth Company (Contract, Australia)',
         position: 'Full-stack Developer',
-        duration: 'May, 2024 - February, 2025 (10-month contract)',
+        duration: 'May, 2023 - October, 2024 (18-month contract)',
         points: [
             "Served as a solo full-stack developer responsible for maintaining and upgrading the company’s private web and desktop scraping application.",
             "Designed and implemented data extraction systems to collect and organize information from multiple online platforms.",
@@ -222,10 +232,10 @@ export const experiences = [
         verifyLink: ""
     },
     {
-        id: 3,
+        id: 4,
         company: 'Premium ESOWP Limited',
         position: 'Front-end Developer',
-        duration: 'May, 2023 — April, 2024',
+        duration: 'May, 2022 — April, 2023',
         points: [
             "Worked as a front-end developer, collaborating closely with UI/UX designers and back-end engineers to deliver client solutions.",
             "Contributed to the development of websites and applications for schools and businesses, automating key aspects of their operations.",
